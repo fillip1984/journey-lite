667210310728a51a43cb6f42c8f11f77aca91226
fillip1984 <fillip1984@gmail.com>
2024-06-30 21:57:58 -0400

    added some more minor tweaks to demo tasks

--- src/app/_components/AreaCard.tsx
+++ src/app/_components/AreaCard.tsx
@@ -7,12 +7,21 @@ export default function AreaCard({ area }: { area: Area }) {
   return (
     <motion.div
       variants={staggeredItem}
-      className="min-h-full min-w-full snap-start rounded-xl border border-white md:min-w-[45%]">
+      className="min-h-full min-w-[80%] snap-center rounded-xl border border-white/30 lg:min-w-[45%]">
       <div className="flex items-center justify-between p-1 text-2xl">
         {area.name}
-        <button type="button">
+        <button
+          type="button"
+          className="text-white/30 transition duration-200 ease-in-out hover:text-white">
           <PiDotsThreeCircleVertical className="h-10 w-10" />
         </button>
+      </div>
+      <div className="my-2 flex flex-col gap-1 p-2">
+        {area.tasks?.map((task) => (
+          <span key={task.name}>
+            <input type="checkbox" className="rounded" /> {task.name}
+          </span>
+        ))}
       </div>
     </motion.div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,33 @@ import AreaCard from "./_components/AreaCard";
 
 export type Area = {
   name: string;
+  tasks?: Task[];
+};
+
+export type Task = {
+  name: string;
 };
 
 export default function Home() {
   const [newArea, setNewArea] = useState("");
   const [areas, setAreas] = useState<Area[]>([
-    { name: "Test1" },
+    {
+      name: "Self improvement",
+      tasks: [
+        {
+          name: "Come up with a morning routine",
+        },
+        {
+          name: "Figure out if bullet journal routine is useful",
+        },
+        {
+          name: "Start a yoga/stretching routine",
+        },
+        {
+          name: "Start a running habit",
+        },
+      ],
+    },
     { name: "Test2" },
     { name: "Test3" },
     { name: "Test4" },
@@ -35,17 +56,17 @@ export default function Home() {
       <div className="flex w-full justify-center">
         <h2>Journey lite</h2>
       </div>
-      <div className="my-2 flex">
+      <div className="my-2 flex justify-center">
         <input
           type="search"
           value={newArea}
           onChange={(e) => setNewArea(e.target.value)}
-          className="w-1/4 rounded-r-none p-2 text-black"
+          className="w-full rounded-r-none p-2 text-black md:w-1/2"
         />
         <button
           type="button"
           onClick={handleAddArea}
-          className="rounded-r bg-success px-4 py-2 text-2xl">
+          className="text-nowrap rounded-r bg-success px-4 py-2 text-xl">
           Add Area
         </button>
       </div>
@@ -53,7 +74,7 @@ export default function Home() {
         variants={staggerContainer}
         initial="hidden"
         animate="show"
-        className="flex flex-1 snap-x snap-mandatory gap-2 overflow-auto p-4">
+        className="flex flex-1 snap-x snap-mandatory gap-2 overflow-auto py-4">
         {areas.map((area) => (
           <AreaCard key={area.name} area={area} />
         ))}
